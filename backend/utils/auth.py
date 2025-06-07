8c072c51c6ae28d714c171df9f3f1651b68caffa
ymiki <y.mozuku.m@gmail.com>
2025-06-07 18:20:58 +0900

    get_current_user_id()関数を作成

--- backend/utils/auth.py
+++ backend/utils/auth.py
@@ -2,10 +2,31 @@
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from db_control.connect import get_db
-from db_control.models import User
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import requests
+
+security = HTTPBearer()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
+
+# アクセストークンからユーザーIDを取得
+def get_current_user_id(credentials: HTTPAuthorizationCredentials = Depends(security)) -> str:
+    token = credentials.credentials
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "apikey": SUPABASE_API_KEY
+    }
+    response = requests.get(f"{SUPABASE_URL}/auth/v1/user", headers=headers)
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired token")
+
+    user_data = response.json()
+    return user_data.get("id")
