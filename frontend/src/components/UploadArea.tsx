263f72f026ceb79cbbc52d4e2b686fd380b0db49
sakagawa <stream.yasuaki@gmail.com>
2025-06-15 23:37:10 +0900

    新規作成する際に、minutesをresetするように修正。

--- frontend/src/components/UploadArea.tsx
+++ frontend/src/components/UploadArea.tsx
@@ -3,12 +3,14 @@ import React, { useRef, useState } from 'react';
 import { UploadIcon } from './UploadIcon';
 import { SmallUploadIcon } from './SmallUploadIcon';
 import { useRouter } from 'next/navigation';
+import { useMinutes } from '@/contexts/MinutesContext';
 
 export const UploadArea: React.FC = () => {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [uploadedFile, setUploadedFile] = useState<File | null>(null);
   const [uploading, setUploading] = useState(false);
   const router = useRouter();
+  const {resetMinutes} = useMinutes();
 
   const handleButtonClick = () => {
     fileInputRef.current?.click();
@@ -50,6 +52,7 @@ export const UploadArea: React.FC = () => {
     });
 
     if (res.ok) {
+      resetMinutes();
       const data = await res.json();
       if (data.status === 'queued') {
         const id = data.minutes_id
